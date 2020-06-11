a5f045a24a5b0f788307345434bbda11b4a90417
Ana Cobian <ana.naiboc@gmail.com>
2020-06-11 15:07:57 -0700

    Update viral-runall.sh

--- viral-runall.sh
+++ viral-runall.sh
@@ -15,7 +15,17 @@
 #7) Go to your working directory (LILAC in this case) 
 #8) Run the command: bash viral-runall.sh IDS.txt /home/acobian/LILAC 40
 
-mkdir $2/P01_prinseq_output
+
+# PART1: quality filtering 
+#mkdir $2/P01_prinseq_output
 #1.- Quality filtering pair end : prinseq++
-cat $1 | xargs -t -I{fileID} sh -c "prinseq++ -fastq $2/P00_raw/{fileID}_R1_001.fastq -fastq2 $2/P00_raw/{fileID}_R2_001.fastq -lc_entropy=0.5 -trim_qual_right=15 -trim_qual_left=15 -trim_qual_type mean -trim_qual_rule lt -trim_qual_window 2 -min_len 30 -min_qual_mean 20  -rm_header -out_name $2/P01_prinseq_output/{fileID} -threads $3 -out_format 1"
+#cat $1 | xargs -t -I{fileID} sh -c "prinseq++ -fastq $2/P00_raw/{fileID}_R1_001.fastq -fastq2 $2/P00_raw/{fileID}_R2_001.fastq -lc_entropy=0.5 -trim_qual_right=15 -trim_qual_left=15 -trim_qual_type mean -trim_qual_rule lt -trim_qual_window 2 -min_len 30 -min_qual_mean 20  -rm_header -out_name $2/P01_prinseq_output/{fileID} -threads $3 -out_format 1"
 #prinseq++ -fastq /home/acobian/LILAC/P00_raw/AC5204_S63_R1_001.fastq -fastq2 /home/acobian/LILAC/P00_raw/AC5204_S63_R2_001.fastq -lc_entropy=0.5 -trim_qual_right=15 -trim_qual_left=15 -trim_qual_type mean -trim_qual_rule lt -trim_qual_window 2 -min_len 30 -min_qual_mean 20  -rm_header -out_name /home/acobian/LILAC/P01_prinseq_output/AC5204_S63 -threads 40-out_format 1
+
+# Reads to use in FRAP, good quality single end
+mkdir $2/P02_for_FRAP
+cat $1 | xargs -t -I{fileID} sh -c "cp $2/P01_prinseq_output/{fileID}_good_out_R1 $2/P02_for_FRAP/"
+
+# PART3: FRAP vs viral refseq
+mkdir $2/P03_FRAP_viralrefseq
+perl jmf4.pl 
